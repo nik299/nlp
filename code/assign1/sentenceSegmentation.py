14c3a3ff8c4a57adda99f7f176c5e56469022244
nik299 <ee17b138@smail.iitm.ac.in>
2020-02-22 19:38:13 +0530

    done sentenceSegmentation.py

--- code/assign1/sentenceSegmentation.py
+++ code/assign1/sentenceSegmentation.py
@@ -1,19 +1,18 @@
-from util import *
+from nltk.tokenize import punkt
+import json
 
 # Add your import statements here
 
 
-
-
 class SentenceSegmentation():
 
-	def naive(self, text):
-		"""
+    def naive(self, text):
+        """
 		Sentence Segmentation using a Naive Approach
 
 		Parameters
 		----------
-		arg1 : str
+		text : str
 			A string (a bunch of sentences)
 
 		Returns
@@ -22,23 +21,21 @@ class SentenceSegmentation():
 			A list of strings where each string is a single sentence
 		"""
 
-		segmentedText = None
+        segmentedText = text.replace('.', '.<>').split('<>')
+        if '' in segmentedText:
+            segmentedText.remove('')
 
-		#Fill in code here
+        # Fill in code here
 
-		return segmentedText
+        return segmentedText
 
-
-
-
-
-	def punkt(self, text):
-		"""
+    def punkt(self, text):
+        """
 		Sentence Segmentation using the Punkt Tokenizer
 
 		Parameters
 		----------
-		arg1 : str
+		text : str
 			A string (a bunch of sentences)
 
 		Returns
@@ -46,9 +43,25 @@ class SentenceSegmentation():
 		list
 			A list of strings where each strin is a single sentence
 		"""
+        sent_splitter = punkt.PunktSentenceTokenizer()
+        segmentedText = sent_splitter.tokenize(text)
 
-		segmentedText = None
+        # Fill in code here
 
-		#Fill in code here
-		
-		return segmentedText
+        return segmentedText
+
+
+# this part is used to test the above functions
+if __name__ == "__main__":
+    queries_json = json.load(open('/home/nikhil/PycharmProjects/nlp/cranfield/cran_queries.json', 'r'))[:]
+    queries = [item["query"] for item in queries_json]
+    count =0
+    for query in queries:
+        segmenter = SentenceSegmentation()
+        naive_res = segmenter.naive(query)
+        punkt_res = segmenter.punkt(query)
+        if naive_res != punkt_res:
+            count += 1
+            print(naive_res)
+            print(punkt_res)
+    print('ratio of matched:'+str(count)+'/'+str(len(queries)))
